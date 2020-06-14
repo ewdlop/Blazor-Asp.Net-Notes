b5bda78a29fba2ad6f3d023a4bc8e95eb14e223f
ewdlop <ray810815@gmail.com>
2020-06-13 20:58:54 -0400

    Add xUnit Unit Testing Project to solution

---
+++ BlazorServerApp.Test/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,31 @@
+﻿using BlazorServerApp.Services;
+using Xunit;
+
+namespace BlazorServerApp.Test
+{
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould() {
+            _primeService = new PrimeService();
+        }
+
+        [Fact]
+        public void IsPrime_InputIs1_ReturnFalse() {
+            var result = _primeService.IsPrime(1);
+
+            Assert.False(result, "1 should not be prime");
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value) {
+            var result = _primeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+    }
+}
---
+++ Services/PrimeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerApp.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate) {
+            if (candidate < 2) {
+                return false;
+            }
+            throw new NotImplementedException("Not fully implemented.");
+        }
+    }
+}
