3613865cd3358928bc30f684028610baa7d254e0
ewdlop <ray810815@gmail.com>
2020-07-01 22:21:17 -0400

    Add role authorzation requirement for changing password

---
+++ Claims/Roles/CustomRoleTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorServerApp.Claims.Roles
+{
+    public static class CustomRoleTypes
+    {
+        public const string SuperAdmin = "SuperAdmin";
+        public const string Admin = "Admin";
+        public const string User = "Public";
+    }
+}
